98685da82b2ac82a184d61768ca6971aed904876
Iorgen Wildrik <w.drik@outlook.com>
2019-11-10 16:17:52 -0300

    Estilização dos componentes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from "react";
 
 import GlobalStyle from "./styles/global";
+import Header from "./components/Header";
+import Board from "./components/Board";
 
 function App() {
-  return <GlobalStyle />;
+  return (
+    <>
+      <Header />
+      <Board />
+
+      <GlobalStyle />
+    </>
+  );
 }
 
 export default App;
--- src/styles/global.js
+++ src/styles/global.js
@@ -7,7 +7,7 @@ export default createGlobalStyle`
     margin: 0;
     padding: 0;
     outline: 0;
-    box-sizing: box;
+    box-sizing: border-box
   }
 
   html, body, #root {
